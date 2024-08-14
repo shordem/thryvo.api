7f79ed42a4b74aa7b0f2782e3fd21aac467c0844
Horlakz <officialhorlakz@gmail.com>
2024-08-14 22:14:01 +0100

    feat(core router) adds endpoint to interact with folder and file handler methods

--- router/core.go
+++ router/core.go
@@ -8,21 +8,43 @@ import (
 	"github.com/shordem/api.thryvo/lib/constants"
 	"github.com/shordem/api.thryvo/lib/database"
 	"github.com/shordem/api.thryvo/middleware"
+	core_repository "github.com/shordem/api.thryvo/repository/core"
+	user_repository "github.com/shordem/api.thryvo/repository/user"
+	core_service "github.com/shordem/api.thryvo/service/core"
 )
 
 func InitializeCoreRouter(router fiber.Router, db database.DatabaseInterface, env constants.Env) {
+	// config
+	fileConfig := config.NewFileConfig(env)
+
+	// repository
+	fileRepository := core_repository.NewFileRepository(db)
+	folderRepository := core_repository.NewFolderRepository(db)
+	userRepository := user_repository.NewUserRepository(db)
+
 	// service
-	mediaService := config.NewFileHelper(env)
+	fileService := core_service.NewFileService(fileConfig, fileRepository, userRepository)
+	folderService := core_service.NewFolderService(folderRepository, userRepository)
 
 	// handler
-	mediaHandler := core_handler.NewMediaHandler(mediaService)
+	fileHandler := core_handler.NewFileHandler(fileService)
+	folderHandler := core_handler.NewFolderHandler(folderService)
 
 	// Middlewares
 	authMiddleware := middleware.Protected()
+	apiKeyMiddleware := middleware.RequireAPIKey(db)
 
 	// Base routes
-	mediaRouter := router.Group("/media")
+	fileRouter := router.Group("/file")
+	folderRouter := router.Group("/folder")
 
-	mediaRouter.Post("/upload", mediaHandler.UploadMedia, authMiddleware)
-	mediaRouter.Get("/:key", mediaHandler.GetMedia)
+	fileRouter.Post("/upload", apiKeyMiddleware, fileHandler.UploadFile)
+	fileRouter.Get("/", authMiddleware, fileHandler.GetUserFiles)
+	fileRouter.Get("/:key", fileHandler.GetFile)
+
+	folderRouter.Post("/", authMiddleware, folderHandler.CreateFolder)
+	folderRouter.Get("/", authMiddleware, folderHandler.GetUserFolders)
+	folderRouter.Get("/:parent_id", authMiddleware, folderHandler.GetFoldersByParent)
+	folderRouter.Put("/:id", authMiddleware, folderHandler.UpdateFolder)
+	folderRouter.Delete("/:id", authMiddleware, folderHandler.DeleteFolder)
 }
