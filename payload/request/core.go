1ed692a255af60b3b708fe547e17657101c5c87f
Horlakz <officialhorlakz@gmail.com>
2024-08-14 22:11:42 +0100

    feat(core request): creates structs for folder creation and update

--- payload/request/core.go
+++ payload/request/core.go
@@ -2,45 +2,11 @@ package request
 
 import "github.com/google/uuid"
 
-type CreateCategoryRequest struct {
-	Name        string     `json:"name"`
-	Description string     `json:"description"`
-	ParentID    *uuid.UUID `json:"parent_id"`
+type CreateFolderRequest struct {
+	Name     string     `json:"name"`
+	ParentID *uuid.UUID `json:"parent_id"`
 }
 
-type UpdateCategoryRequest struct {
-	CreateCategoryRequest
-}
-
-type CreateProductRequest struct {
-	CategoryUUID  string            `json:"category_id"`
-	Name          string            `json:"name"`
-	Description   string            `json:"description"`
-	Specification string            `json:"specification"`
-	Price         int               `json:"price"`
-	Stock         int               `json:"stock"`
-	SlashPrice    int               `json:"slash_price"`
-	Images        []string          `json:"images"`
-	Discounts     []DiscountRequest `json:"discounts"`
-	// ProductVariations []struct {
-	// 	Name  string `json:"name"`
-	// 	Price int    `json:"price"`
-	// 	Stock int    `json:"stock"`
-	// } `json:"product_variations"`
-}
-
-type DiscountRequest struct {
-	Discount int `json:"discount"`
-	Min      int `json:"min"`
-	Max      int `json:"max"`
-}
-
-type CreateWishlistRequest struct {
-	ProductUUID string `json:"product_id"`
-}
-
-type CreateReviewRequest struct {
-	ProductID string `json:"product_id"`
-	Rating    int    `json:"rating"`
-	Comment   string `json:"comment"`
+type UpdateFolderRequest struct {
+	CreateFolderRequest
 }
