9f64ba969afa90a7b41128fce904bc18715bd0b3
Horlakz <officialhorlakz@gmail.com>
2024-08-14 22:05:11 +0100

    enhancement(core model): updates model fields

--- model/core.go
+++ model/core.go
@@ -9,9 +9,23 @@ import (
 type File struct {
 	database.BaseModel
 
-	UserID       uuid.UUID `json:"user_id"`
-	Key          string    `json:"key"`
-	OriginalName string    `json:"original_name"`
-	Type         string    `json:"type"`
-	Size         int64     `json:"size"`
+	UserID       uuid.UUID  `json:"user_id"`
+	FolderID     *uuid.UUID `json:"folder_id"`
+	Key          string     `json:"key"`
+	OriginalName string     `json:"original_name"`
+	MimeType     string     `json:"type"`
+	Size         int64      `json:"size"`
+	Visibility   string     `json:"visibility"`
+
+	Folder *Folder `json:"folder"`
+}
+
+type Folder struct {
+	database.BaseModel
+
+	UserID   uuid.UUID  `json:"user_id"`
+	ParentID *uuid.UUID `json:"parent_id"`
+	Name     string     `json:"name"`
+
+	Parent *Folder `json:"parent"`
 }
